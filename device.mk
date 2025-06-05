7b73e5724eb25dbcebff8d638bfb0b0811660b47
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:30:09 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -36,3 +36,4 @@ include kernel/amlogic/kernel-modules/dhd-driver/firmware/wifi/wifi.mk
 
 ## Inherit from the common tree product makefile
 $(call inherit-product, device/amlogic/g12-common/g12.mk)
+$(call inherit-product, vendor/lindroid/lindroid.mk)
